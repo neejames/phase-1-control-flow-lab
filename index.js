7527a8a4ca5cf97394a717be18429a4d5068d1b1
Neechelle James <neechelle.james@gmail.com>
2023-04-10 20:55:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){ 
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  }
+  else if (400 < someValue && someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (2000 < someValue && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else (someValue) 
+    return 'No can do.'
+     
+  }
+function ternaryCheckCity(cityName){
+  
+  return (cityName === 'NYC' ? "Ok, sounds good.": 'No go.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch (tip) {
+    case 'generous':
+     message = 'Thank you so much.';
+     break;
+   case 'not as generous':
+    message = 'Thank you.';
+    break;
+    default:
+      message = 'Bye.'
+      break;
+  }
+  return message;
 }
